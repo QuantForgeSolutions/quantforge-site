fd2c2887c160620c81c46abb20396aa2dcbf480a
Cramer-Lawrence <cramer.lawrence@msn.com>
2025-05-06 15:39:36 -0600

    Added under construction flag

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
-console.log('QuantForge site loaded');
+const siteUnderConstruction = true;
+
+if (siteUnderConstruction) {
+    document.body.innerHTML = `
+    <div class="min-h-screen flex flex-col justify-center items-center bg-gray-100 dark:bg-gray-900 text-center text-gray-800 dark:text-white">
+      <h1 class="text-4xl font-bold mb-4">🚧 Site Under Construction 🚧</h1>
+      <p class="text-lg">We're working hard to launch something awesome. Check back soon!</p>
+    </div>
+  `;
+}
