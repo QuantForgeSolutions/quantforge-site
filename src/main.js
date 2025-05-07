37142d1235092e2129bb2af536e6e48909bd261d
Cramer-Lawrence <cramer.lawrence@msn.com>
2025-05-07 11:14:03 -0600

    Added company name to under construction page

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ const siteUnderConstruction = true;
 if (siteUnderConstruction) {
     document.body.innerHTML = `
     <div class="min-h-screen flex flex-col justify-center items-center bg-gray-100 dark:bg-gray-900 text-center text-gray-800 dark:text-gray-200">
+      <h1 class="text-6xl mb-8">QuantForge Solutions</h1>
       <h1 class="text-4xl font-bold mb-4">🚧 Site Under Construction 🚧</h1>
       <p class="text-lg">We're working hard to launch something awesome. Check back soon!</p>
     </div>
